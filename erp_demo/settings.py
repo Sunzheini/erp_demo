cf7c261cf22d4ad676327e1b349057f2d92f4b72
Sunzheini <daniel_zorov@abv.bg>
2023-01-03 14:27:43 +0200

    update staticfiles

--- erp_demo/settings.py
+++ erp_demo/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from os.path import join
 from pathlib import Path
 
@@ -114,9 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    join(BASE_DIR, 'staticfiles'),
+    join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
