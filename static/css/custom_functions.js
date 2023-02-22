b6324de5cc514cd1e742407a7045844cd2d2ea99
Sunzheini <daniel_zorov@abv.bg>
2023-02-22 18:15:05 +0200

    frontend updates ongoing

---
+++ static/css/custom_functions.js
@@ -0,0 +1,26 @@
+        function fShowHideProcessForm() {
+            let x = document.getElementById("process_form");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+
+        function fShowHideProcessStepForm() {
+            let x = document.getElementById("process_step_form");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+
+        function fShowHideRequirementForm() {
+            let x = document.getElementById("requirement_form");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
