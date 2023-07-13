5b9503287becd00f6a5fd632be100088ed2f1c6f
Sunzheini <daniel_zorov@abv.bg>
2023-07-13 17:51:59 +0300

    fixed a naming issue, started tests structure

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+"""
+erp_demo                            (django project)
+    actionplan_mng                  (django app)
+        views.py                    (.py file)
+            def index(request):     (function)
+            def create(request):    (function)
+            def show(request):      (function)
+        forms.py                    (.py file)
+            class CreateForm:       (class)
+
+tests                               (tests project)
+    actionplan_mng                  (package)
+        views                       (package)
+            test_index.py           (.py file)      max 10-15 tests in a file
+            test_create.py          (.py file)
+            test_show.py            (.py file)
+        forms                       (package)
+            test_CreateForm.py     (.py file)
+
+
+        or views.py                 (.py file)      if not much tests
+"""
