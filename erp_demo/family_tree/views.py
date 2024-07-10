58ea0f4a67aca84fbd33c2f468fb6ee6c0e21243
Sunzheini <daniel_zorov@abv.bg>
2024-07-10 13:30:57 +0300

    Started with views

--- erp_demo/family_tree/views.py
+++ erp_demo/family_tree/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from erp_demo.family_tree.models import FamilyTree
 
 
 def index(request):
     return render(request, 'family_tree/index.html')
+
+
+def family_tree(request):
+    context = {
+        'family_tree': FamilyTree.objects.all(),
+    }
+    return render(request, 'family_tree/index.html', context)
